dfe25ec85289ed9098f0ca38310c786b65b20387
Chan Park <qkrcks8028@gmail.com>
2025-09-19 17:32:57 +0900

    procedure 문 추가

---
+++ database/proc_01.py
@@ -0,0 +1,26 @@
+# 데이터베이스 연결
+# 환경변수 로드
+# os를 이용해서 환경변수의 값을 읽어서 connection 객체를 생성
+# 커넥션 객체의 cursor 객체를 생성
+# 커서 객체의 callproc('프로시저이름', [, , , ,])
+# 프로시저 호출
+import pymysql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_connection():
+    return pymysql.connect(
+        host = os.getenv('DB_HOST'),
+        user = os.getenv('DB_USER'),
+        password = os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_TEST_NAME')
+    )
+
+def call_procedure():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.callproc('AddCodeWithTransaction', ['ADDR','서울','서울특별시',0,'Y'])   
+
+call_procedure()
